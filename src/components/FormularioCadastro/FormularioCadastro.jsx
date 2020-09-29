4eed39f0c6e536ecd8dde7014d9810c3eca6dfcf
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-09-29 20:15:24 -0300

    feat: conditional forms with switch case

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import { Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuarios from "./DadosUsuario";
 
 function FormularioCadastro({ onSubmit, validarCPF }) {
+  const [etapaAtual, setEtapaAtual] = useState(1);
 
-  return (
-    <>
-      <DadosPessoais onSubmit={onSubmit} validarCPF={validarCPF} />
-      <DadosUsuarios />
-    </>
-  );
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuarios />;
+      case 1:
+        return <DadosPessoais onSubmit={onSubmit} validarCPF={validarCPF} />;
+      case 2:
+        return <DadosEntrega />;
+      default:
+        return <Typography>Error</Typography>;
+    }
+  }
+
+  return <>{formularioAtual(etapaAtual)}</>;
 }
 
+// <DadosPessoais onSubmit={onSubmit} validarCPF={validarCPF} />
+// <DadosUsuarios />
+// <DadosEntrega />
 export default FormularioCadastro;
